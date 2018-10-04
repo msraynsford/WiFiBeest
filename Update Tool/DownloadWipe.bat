c25bc6c528ab2c113091d1dd3819ee3985da0150
msraynsford <m_raynsford@hotmail.com>
2018-10-04 11:46:54 +0100

    Update Tool

--- Update Tool/DownloadWipe.bat
+++ Update Tool/DownloadWipe.bat
@@ -1,3 +1,5 @@
+esptool.exe -v -cd nodemcu -cb 921600 -cp COM5 -ca 0x000000 -cf wipe.bin
+sleep 3
 esptool.exe -v -cd nodemcu -cb 921600 -cp COM5 -ca 0x000000 -cf d1_mini.bin
 sleep 1
 esptool.exe -v -cd nodemcu -cb 921600 -cp COM5 -ca 0x300000 -cf spiffs.bin
